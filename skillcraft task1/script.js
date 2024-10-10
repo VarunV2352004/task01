ff91c0218d272ea0b1c3bc9b5178ce6122d4f808
VarunV2352004 <varunv7026@gmail.com>
2024-10-10 15:42:48 +0530

    Add files via upload

---
+++ skillcraft task1/script.js
@@ -0,0 +1,11 @@
+// Get the navbar element
+const navbar = document.getElementById('navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
